63fb4ebca9fa0a6ffd56528af2ac940e59be62c6
Marilia Gabriela Pires Matos <mariliagpm@hotmail.com>
2025-05-15 01:19:35 -0300

    first commit

--- tests/RoomsToGoLogin.cs
+++ tests/RoomsToGoLogin.cs
@@ -2,9 +2,11 @@
 using roomstogoseleniumframework.Utilities;
 using NUnit.Framework;
 using NUnit.Framework.Legacy;
+using NUnit.Allure.Core;
 
 namespace roomstogoseleniumframework.tests
 {
+    [AllureNUnit]
     public class RoomsToGoLogin : Base
     {
         [Test, Category("Regression")]
