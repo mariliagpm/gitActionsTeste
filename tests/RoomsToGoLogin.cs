02470a96d7cc54c06d5d1e338e26bea9d609407c
Marilia Gabriela Pires Matos <mariliagpm@hotmail.com>
2025-05-14 23:57:27 -0300

    first commit

---
+++ tests/RoomsToGoLogin.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using roomstogoseleniumframework.Utilities;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace roomstogoseleniumframework.tests
+{
+    public class RoomsToGoLogin : Base
+    {
+        [Test, Category("Regression")]
+        public void TestGetSwiperItems()
+        {
+
+            ClassicAssert.AreEqual(driver.Value.Title, "Google");
+
+
+        }
+
+       
+    }
+}
